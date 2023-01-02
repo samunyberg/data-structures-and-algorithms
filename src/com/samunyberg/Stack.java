5c82d498fa064ddf67043f4cdce340b53d99cde4
Samu Nyberg <samu.nyberg@gmail.com>
2023-01-02 12:08:55 +0200

    Add an implementation of a stack.

---
+++ src/com/samunyberg/Stack.java
@@ -0,0 +1,31 @@
+package com.samunyberg;
+
+public class Stack {
+    private int[] items = new int[5];
+    private int count;
+
+    public void push(int item) {
+        if (count == items.length)
+            throw new StackOverflowError();
+
+        items[count++] = item;
+    }
+
+    public int pop() {
+        if (count == 0)
+            throw new IllegalStateException();
+
+        return items[--count];
+    }
+
+    public int peek() {
+        if (count == 0)
+            throw new IllegalStateException();
+
+        return items[count - 1];
+    }
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+}
