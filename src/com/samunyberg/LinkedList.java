cf50e24f2829e8bc6df1ebf28094475b7b0fdb21
Samu Nyberg <samu.nyberg@gmail.com>
2023-01-05 13:09:49 +0200

    Add an implementation of a linked list.

---
+++ src/com/samunyberg/LinkedList.java
@@ -0,0 +1,114 @@
+package com.samunyberg;
+
+import java.util.NoSuchElementException;
+
+public class LinkedList {
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node first;
+    private Node last;
+    private int size;
+
+    public void addLast(int item) {
+        var node = new Node(item);
+
+        if (isEmpty())
+            first = last = node;
+        else {
+            last.next = node;
+            last = node;
+        }
+
+        size++;
+    }
+
+    public void addFirst(int item) {
+        var node = new Node(item);
+
+        if (isEmpty())
+            first = last = node;
+        else {
+            node.next = first;
+            first = node;
+        }
+
+        size++;
+    }
+
+    public void removeFirst() {
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        if (first == last)
+            first = last = null;
+        else {
+            var second = first.next;
+            first.next = null;
+            first = second;
+        }
+
+        size--;
+    }
+
+    public void removeLast() {
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        if (first == last)
+            first = last = null;
+        else {
+            var previous = getPrevious(last);
+            previous.next = null;
+            last = previous;
+        }
+
+        size--;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int indexOf(int item) {
+        int index = 0;
+        var current = first;
+        while (current != null) {
+            if (current.value == item) return index;
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    public boolean contains(int item) {
+        return indexOf(item) != -1;
+    }
+
+    private boolean isEmpty() {
+        return first == null;
+    }
+
+    private Node getPrevious(Node node) {
+        var current = first;
+        while (current != null) {
+            if (current.next == node) return current;
+            current = current.next;
+        }
+        return null;
+    }
+
+    public void print() {
+        var current = first;
+        while (current != null) {
+            System.out.println(current.value);
+            current = current.next;
+        }
+    }
+}
