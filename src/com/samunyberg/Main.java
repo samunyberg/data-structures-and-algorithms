5e1f0e4e81f42eb56b808d11b6945faa7bb9bcab
Samu Nyberg <samu.nyberg@gmail.com>
2023-03-16 15:25:33 +0200

    Add Tree class (binary search tree) with insert() and find() methods.

--- src/com/samunyberg/Main.java
+++ src/com/samunyberg/Main.java
@@ -2,5 +2,16 @@ package com.samunyberg;
 
 public class Main {
     public static void main(String[] args) {
+        Tree tree = new Tree();
+        tree.insert(7);
+        tree.insert(4);
+        tree.insert(9);
+        tree.insert(1);
+        tree.insert(6);
+        tree.insert(8);
+        tree.insert(10);
+        System.out.println("Done");
+
+        System.out.println(tree.find((1)));
     }
 }
