88d6f7a98582a54c93a5cb0c5b8258959df47cf0
Samu Nyberg <samu.nyberg@gmail.com>
2023-03-16 15:30:16 +0200

    Add traversePreOrder() method to Tree class.

--- src/com/samunyberg/Main.java
+++ src/com/samunyberg/Main.java
@@ -10,8 +10,7 @@ public class Main {
         tree.insert(6);
         tree.insert(8);
         tree.insert(10);
-        System.out.println("Done");
 
-        System.out.println(tree.find((1)));
+        tree.traversePreOrder();
     }
 }
--- src/com/samunyberg/Tree.java
+++ src/com/samunyberg/Tree.java
@@ -56,6 +56,19 @@ public class Tree {
         }
         return false;
     }
+
+    public void traversePreOrder() {
+        traversePreOrder(root);
+    }
+
+    private void traversePreOrder(Node root) {
+        if (root == null)
+            return;
+
+        System.out.println(root.value);
+        traversePreOrder(root.leftChild);
+        traversePreOrder(root.rightChild);
+    }
 }
 
 
