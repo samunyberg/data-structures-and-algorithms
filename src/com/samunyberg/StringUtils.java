cf56c7cc1a8145e9d20d3354de1e68d1e8c052e6
samunyberg <samu.nyberg@gmail.com>
2023-08-10 16:40:29 +0300

    Add areAnagrams() method to StringUtils class (implemented with sorting).

--- src/com/samunyberg/StringUtils.java
+++ src/com/samunyberg/StringUtils.java
@@ -104,4 +104,17 @@ public class StringUtils {
 
         return String.join(" ", words);
     }
+
+    public static boolean areAnagrams(String first, String second) {
+        if (first == null || second == null || first.length() != second.length())
+            return false;
+
+        var array1 = first.toLowerCase().toCharArray();
+        Arrays.sort(array1);
+
+        var array2 = second.toLowerCase().toCharArray();
+        Arrays.sort(array2);
+
+        return Arrays.equals(array1, array2);
+    }
 }
