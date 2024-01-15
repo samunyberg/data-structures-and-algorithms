ad8ee7f7ddd8745b89ac36fffcd1acc8f8081cbf
samunyberg <samu.nyberg@gmail.com>
2024-01-15 16:33:28 +0200

    Add AVLTree class

---
+++ src/com/samunyberg/AVLTree.java
@@ -0,0 +1,45 @@
+package com.samunyberg;
+
+public class AVLTree {
+    private class AVLNode {
+        private int height;
+        private int value;
+        private AVLNode leftChild;
+        private AVLNode rightChild;
+
+        public AVLNode(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Value=" + this.value;
+        }
+    }
+
+    AVLNode root;
+
+    public void insert(int value) {
+        root = insert(root, value);
+    }
+
+    private AVLNode insert(AVLNode root, int value) {
+        if (root == null)
+            return new AVLNode(value);
+
+        if (value < root.value)
+            root.leftChild = insert(root.leftChild, value);
+        else
+            root.rightChild = insert(root.rightChild, value);
+
+        root.height = Math.max(
+                height(root.leftChild),
+                height(root.rightChild)) + 1;
+
+        return root;
+    }
+
+    private int height(AVLNode node) {
+        return (node == null) ? -1 : node.height;
+    }
+}
