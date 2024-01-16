caf2715177d11da590e16b5a8a3a06284e16c093
samunyberg <samu.nyberg@gmail.com>
2024-01-16 16:46:45 +0200

    Add remove() method to Heap class

--- src/com/samunyberg/Heap.java
+++ src/com/samunyberg/Heap.java
@@ -13,6 +13,22 @@ public class Heap {
         bubbleUp();
     }
 
+    public int remove() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        var root = items[0];
+        items[0] = items[--size];
+
+        bubbleDown();
+
+        return root;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
     public boolean isFull() {
         return size == items.length;
     }
@@ -25,8 +41,64 @@ public class Heap {
         }
     }
 
+    private void bubbleDown() {
+        var index = 0;
+        while (index <= size && !isValidParent(index)) {
+            swap(index, largerChildIndex(index));
+            index = largerChildIndex(index);
+        }
+    }
+
+    private int largerChildIndex(int index) {
+        if (!hasLeftChild(index))
+            return index;
+
+        if (!hasRightChild(index))
+            return leftChildIndex(index);
+
+        return (leftChild(index) > rightChild(index)) ?
+                leftChildIndex(index) :
+                rightChildIndex(index);
+    }
+
+    private boolean hasLeftChild(int index) {
+        return leftChildIndex(index) <= size;
+    }
+
+    private boolean hasRightChild(int index) {
+        return rightChildIndex(index) <= size;
+    }
+
+    private boolean isValidParent(int index) {
+        if (!hasLeftChild(index))
+            return true;
+
+        var isValid = items[index] >= leftChild(index);
+
+        if (hasRightChild(index))
+            isValid &= items[index] >= rightChild(index);
+
+        return isValid;
+    }
+
+    private int leftChild(int index) {
+        return items[leftChildIndex(index)];
+    }
+
+    private int rightChild(int index) {
+        return items[rightChildIndex(index)];
+    }
+
     private int parent(int index) {
         return (index - 1) / 2;
+    }
+
+    private int leftChildIndex(int index) {
+        return (index * 2) + 1;
+    }
+
+    private int rightChildIndex(int index) {
+        return (index * 2) + 2;
     }
 
     private void swap(int first, int second) {
